8cda7b55c8d1dcf05542d0b756c346f4b2e1b860
Yasunori Shimura <yasu0710@gmail.com>
2021-09-09 14:37:35 +0900

    made capture_image.py uses directory setting in config.py

--- capture_image.py
+++ capture_image.py
@@ -1,4 +1,8 @@
+import os
+
 import cv2
+
+from config import *
 
 cam_w = 640
 cam_h = 480
@@ -7,13 +11,14 @@ cap=cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH , cam_w)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, cam_h)
 
-print('input number:', end='')
-number=input()
-print('input name:', end='')
+print('Input ID number:', end='')
+id_number=input()
+print('Input name:', end='')
 name=input()
 
 print('Hit \'(SPACE)\' key to capture an image.')
 print('Hit \'(ESC)\' key to exit.')
+print('Note: \'cam\' window must be active (selected)')
 key=0
 num=0
 while key!=27:  #ESC key to exit
@@ -21,7 +26,7 @@ while key!=27:  #ESC key to exit
     cv2.imshow('cam', img)
     key = cv2.waitKey(1)
     if key==ord(' '):
-        fn = './face_db/{}-{}-{}.jpg'.format(number, name, num)
+        fn = os.path.join(image_dir, '{}-{}-{}.jpg'.format(id_number, name, num))
         cv2.imwrite(fn, img)
         print('captured:', fn)
         num+=1
