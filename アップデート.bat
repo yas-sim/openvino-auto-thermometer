201e53780ca5b53de4d014c25a5f093302702629
Yasunori Shimura <yasu0710@gmail.com>
2021-11-22 14:21:25 +0900

    update updator

--- アップデート.bat
+++ アップデート.bat
@@ -1,4 +1,13 @@
-@echo off
+rem @echo off
+for /f "usebackq delims=" %%A in (`python -c "import os; print(os.getcwd().split(os.sep)[-1])"`) do set dirname=%%A
+if not exist .git (
+    cd ..
+    git clone https://github.com/yas-sim/openvino-auto-thermometer clone-temp
+    xcopy /E /H clone-temp %dirname%
+    cd %dirname%
+)
 git pull
-git checkout master
+git checkout excel_operation
 pause
+
+:end
